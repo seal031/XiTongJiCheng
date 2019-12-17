8f1f565c9c7befea4e0b1d8f10ee25e52fec66fb
QIKEXIN <87749356@qq.COM>
2019-12-17 10:41:37 +0800

    1

---
+++ ChangChunMenJin/SqlServerHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ChangChunMenJin
+{
+    public class SqlServerHelper
+    {
+        private static string sqlconStr;
+        static SqlServerHelper()
+        {
+            SqlConnectionStringBuilder scsb = new SqlConnectionStringBuilder();
+            scsb.DataSource = ConfigWorker.GetConfigValue("sqlserverIp");
+            scsb.IntegratedSecurity = true;
+            scsb.InitialCatalog = ConfigWorker.GetConfigValue("sqlserverDbName");
+            scsb.UserID = ConfigWorker.GetConfigValue("sqlserverUser");
+            scsb.Password = ConfigWorker.GetConfigValue("sqlserverPassword");
+            sqlconStr = scsb.ConnectionString;
+        }
+        public static List<DbPerson> getPersons()
+        {
+            List<DbPerson> persons = new List<ChangChunMenJin.DbPerson>();
+            using (SqlConnection con = new SqlConnection(sqlconStr))
+            {
+                SqlCommand cmd = new SqlCommand("select * from persons", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    
+                }
+            }
+            return persons;
+        }
+    }
+
+    public class DbPerson
+    {
+
+    }
+}
